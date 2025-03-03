3cd469691f313f61c29cf08351cf6ce9632d759f
Thaipham <phamthithai129@gmail.com>
2025-03-03 11:22:07 -0800

    Add extract_data_to_df function

--- spotify_api.py
+++ spotify_api.py
@@ -1,12 +1,15 @@
 import requests
 import base64
 import json 
-from config import CLIENT_ID, CLIENT_SECRET
+import pandas as pd
+import logging
+from dotenv import load_dotenv
+import os
 
 def get_access_token():
-    """
-    Fetches an access token using the Client Credentials Flow.
-    """
+    load_dotenv()
+    CLIENT_ID = os.getenv("CLIENT_ID")
+    CLIENT_SECRET = os.getenv("CLIENT_SECRET")
     token_url = 'https://accounts.spotify.com/api/token'
     headers = {
         'Authorization': f"Basic {base64.b64encode(f'{CLIENT_ID}:{CLIENT_SECRET}'.encode()).decode()}",
@@ -20,13 +23,9 @@ def get_access_token():
         token_data = response.json()
         return token_data['access_token']
     else:
-        print(f"Error: {response.status_code} - {response.text}")
-        return None
+        logging.error(f"Error: {response.status_code} - {response.text}")
 
 def get_album_info(album_id, access_token):
-    """
-    Fetches all album information using the Spotify API.
-    """
     url = f'https://api.spotify.com/v1/albums/{album_id}'
     headers = {
         'Authorization': f'Bearer {access_token}'
@@ -34,21 +33,15 @@ def get_album_info(album_id, access_token):
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         album_data = response.json()
-        
-        # Convert all list fields to JSON strings
         for key, value in album_data.items():
             if isinstance(value, (list, dict)): 
                 album_data[key] = json.dumps(value)  
         
         return album_data
     else:
-        print(f"Error fetching album {album_id}: {response.status_code} - {response.text}")
-        return None
+         logging.error(f"Error fetching album {album_id}: {response.status_code} - {response.text}")
 
 def get_artist_info(artist_id, access_token):
-    """
-    Fetches all artist information using the Spotify API.
-    """
     url = f'https://api.spotify.com/v1/artists/{artist_id}'
     headers = {
         'Authorization': f'Bearer {access_token}'
@@ -56,13 +49,27 @@ def get_artist_info(artist_id, access_token):
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         artist_data = response.json()
-        
-        # Convert all list fields to JSON strings
         for key, value in artist_data.items():
             if isinstance(value, (list, dict)):  
                 artist_data[key] = json.dumps(value)  
         
         return artist_data
     else:
-        print(f"Error fetching artist {artist_id}: {response.status_code} - {response.text}")
-        return None
+         logging.error(f"Error fetching artist {artist_id}: {response.status_code} - {response.text}")
+
+def extract_album_data_to_df(album_ids, access_token):
+   albums_data = []
+   for album_id in album_ids:
+        album_info = get_album_info(album_id, access_token)
+        if album_info:
+            albums_data.append(album_info)
+   return pd.DataFrame(albums_data)
+
+def extract_artist_data_to_df(artist_ids, access_token):
+    artists_data = []
+    for artist_id in artist_ids:
+        artist_info = get_artist_info(artist_id, access_token)
+        if artist_info:
+            artists_data.append(artist_info)
+    return pd.DataFrame(artists_data)
+
