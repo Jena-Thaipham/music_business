b7fc76f669ca3cdfc892b3e1b19a416f75407578
Thaipham <phamthithai129@gmail.com>
2025-03-03 11:21:18 -0800

    Add create_database function

--- file_utils.py
+++ file_utils.py
@@ -1,6 +1,5 @@
-import csv
+import sqlite3
 
-#Read IDs from txt file
 def read_ids_from_txt(file_path):
     ids = []
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -10,15 +9,18 @@ def read_ids_from_txt(file_path):
                 ids.append(line)
     return ids
 
-#Save to csv file
-def save_to_csv(data, filename):
-    if not data:
-        print(f"No data to save for {filename}.")
-        return
 
-    # Extract headers dynamically from the first item in the data
-    fieldnames = data[0].keys()
-    with open(filename, mode='w', newline='', encoding='utf-8') as file:
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(data)
+def create_database():
+    connection = sqlite3.connect("spotify_db/spotify.db")
+    cursor = connection.cursor()
+
+    with open('spotify_db/schema/create_album_schema.sql', 'r') as file:
+        create_album_table_query = file.read()
+    cursor.execute(create_album_table_query)
+
+    with open('spotify_db/schema/create_artist_schema.sql', 'r') as file:
+        create_artist_table_query = file.read()
+    cursor.execute(create_artist_table_query)
+
+    connection.commit()
+    return connection
