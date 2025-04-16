1e030999c0780da7a16acbe1fd98c6a6a8a87adc
Thaipham <phamthithai129@gmail.com>
2025-04-16 09:53:41 -0700

    Remove unnecessary files from PR for better focus on changes

--- spotify_db/schema/create_artist_schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `artists` (
-	`artist_id` TEXT NOT NULL PRIMARY KEY,
-	`artist_name` TEXT,
-	`genre` TEXT,
-	`popularity` INTEGER CHECK(popularity BETWEEN 0 AND 100),
-	`followers` INTEGER,
-	`artist_uri` TEXT 
-);
--- spotify_db/schema/create_user_schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `users` (
-            `user_id` TEXT NOT NULL UNIQUE PRIMARY KEY,
-            `country` TEXT,
-            `followers` INTEGER ,
-            `user_uri` TEXT 
-        );
-        
