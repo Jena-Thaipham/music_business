8ded26e1f069f89e699b887c62c8cd7006a0db83
Thaipham <phamthithai129@gmail.com>
2025-04-16 10:18:35 -0700

    Change release_date column type from REAL to TEXT

--- spotify_db/schema/create_album.sql
+++ spotify_db/schema/create_album.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS albums (
     album_name TEXT,
     album_type TEXT,
     artist_id TEXT NOT NULL,
-    release_date REAL,
+    release_date TEXT,
     total_tracks INTEGER,
     popularity INTEGER CHECK(popularity BETWEEN 0 AND 100),
     markets TEXT,
